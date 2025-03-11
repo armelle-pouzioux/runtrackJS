d438ebd00270a780db79eb3120b0f4591e504357
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2025-03-11 10:17:13 +0100

    getElementById().textContent

---
+++ jour02/job01/script.js
@@ -0,0 +1,6 @@
+function citation() {
+    let texteCitation = document.getElementById("citation").innerHTML;
+    console.log(texteCitation);
+}
+
+document.getElementById("button").addEventListener("click", citation);
