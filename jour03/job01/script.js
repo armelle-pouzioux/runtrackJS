c0f70002b77cb00a1ad2986085394c51eb462ed7
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2025-03-12 11:58:15 +0100

    jquery $ .click .fadeToggle .is .text

---
+++ jour03/job01/script.js
@@ -0,0 +1,10 @@
+$("#toggle").click(function() {
+    $("#texte").fadeToggle();
+
+    if ($("#texte").is(":visible")) {
+        $("#toggle").text("Cacher la citation");
+    } else {
+        $("#toggle").text("Afficher la citation");
+    }
+});
+    
