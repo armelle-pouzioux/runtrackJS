7decb03e5ab07dfdaec9ae2bf5c8f3aa2befb4f9
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2025-03-13 14:49:24 +0100

    fetch files.txt

--- jour04/job01/script.js
+++ jour04/job01/script.js
@@ -1,4 +1,12 @@
 document.getElementById("button").addEventListener("click", async function() {
+    let container = document.getElementById("expressionContainer");
+
+    // Si le paragraphe est déjà visible, on le cache
+    if (container.style.display === "block") {
+        container.style.display = "none";
+        return;
+    }
+
     try {
         let response = await fetch("expression.txt");
 
@@ -10,11 +18,12 @@ document.getElementById("button").addEventListener("click", async function() {
 
         let p = document.createElement("p");
         p.textContent = text;
-
-        document.getElementById("expressionContainer").innerHTML = "";
-        document.getElementById("expressionContainer").appendChild(p);
+        container.innerHTML = `<p>${text}</p>`;
+        container.style.display = "block"; // On l'affiche
         
     } catch (error) {
         console.error("Erreur :", error);
     }
+
+    
 });
